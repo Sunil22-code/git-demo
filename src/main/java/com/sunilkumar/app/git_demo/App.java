53aee43bc78a5f569a2c158bbc7ab357ef1c395d
Sunil22-code <sunik221994@gmail.com>
2020-10-08 15:50:57 +0530

    added if condition to check the data

--- src/main/java/com/sunilkumar/app/git_demo/App.java
+++ src/main/java/com/sunilkumar/app/git_demo/App.java
@@ -6,6 +6,15 @@ package com.sunilkumar.app.git_demo;
  */
 public class App 
 {
+	public void addData(String data)
+	{
+		System.out.println( "Data=" + data );
+		
+		if(data.equals("java"))
+		{
+			System.out.println( "Hello World!" );	
+		}
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
