3156da12336e115c6f44c18cffc4ca99b4d96880
Sunil Golu <72550563+Sunil22-code@users.noreply.github.com>
2020-10-08 17:08:18 +0530

    method added by golu

--- src/main/java/com/sunilkumar/app/git_demo/App.java
+++ src/main/java/com/sunilkumar/app/git_demo/App.java
@@ -9,6 +9,10 @@ package com.sunilkumar.app.git_demo;
 			System.out.println( "Hello World!" );	
 		}
 	}
+	 public void newMethod()
+	 {
+	    System.out.println( "Hello" );	 
+	 }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
